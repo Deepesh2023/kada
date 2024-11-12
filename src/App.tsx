627a56e48381ff0008e70ff61a0f8ef740cec44d
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-11-12 16:31:29 +0530

    added a simple navbar with dummy or non working links to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import "./App.css";
+
 export default function App() {
   return (
-    <>
-      <h1>Kada</h1>
-    </>
+    <div class="container">
+      <Navigation />
+      <div class="content"></div>
+    </div>
+  );
+}
+
+function Navigation() {
+  return (
+    <nav class="navbar">
+      <ul>
+        <li>
+          <a href="">Sale/service</a>
+        </li>
+        <li>
+          <a href="">Manage stock</a>
+        </li>
+        <li>
+          <a href="">History</a>
+        </li>
+        <li>
+          <a href="">Stats</a>
+        </li>
+      </ul>
+
+      <div>
+        <a href="">Settings</a>
+      </div>
+    </nav>
   );
 }
