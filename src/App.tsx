c0732a4db444c89bd5d4857755e660d61504a135
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-11-12 17:15:24 +0530

    added routes for the navbar links a added placeholder pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { Route, Router } from "@solidjs/router";
 import "./App.css";
+
+import SalesService from "./pages/SalesService";
+import ManageStocks from "./pages/ManageStocks";
+import History from "./pages/History";
+import Stats from "./pages/Stats";
+import Settings from "./pages/Settings";
 
 export default function App() {
   return (
@@ -14,21 +21,21 @@ function Navigation() {
     <nav class="navbar">
       <ul>
         <li>
-          <a href="">Sale/service</a>
+          <a href="/sales-service">Sales/service</a>
         </li>
         <li>
-          <a href="">Manage stock</a>
+          <a href="/manage-stocks">Manage stocks</a>
         </li>
         <li>
-          <a href="">History</a>
+          <a href="/history">History</a>
         </li>
         <li>
-          <a href="">Stats</a>
+          <a href="/stats">Stats</a>
         </li>
       </ul>
 
       <div>
-        <a href="">Settings</a>
+        <a href="/settings">Settings</a>
       </div>
     </nav>
   );
@@ -38,6 +45,13 @@ function Page() {
   return (
     <div>
       <h1>Kada</h1>
+      <Router>
+        <Route path={"/sales-service"} component={SalesService} />
+        <Route path={"/manage-stocks"} component={ManageStocks} />
+        <Route path={"/history"} component={History} />
+        <Route path={"/stats"} component={Stats} />
+        <Route path={"/settings"} component={Settings} />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/SalesService.tsx
@@ -0,0 +1,3 @@
+export default function SalesService() {
+  return <>sales and service works</>;
+}
