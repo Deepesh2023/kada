445dee1efcc0132377a68ea2500e263de32249c3
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-11-13 08:56:21 +0530

    started working on the sales-service page and added a togglable button for adding sales/service that am really proud of

--- src/pages/sales-service/SalesService.tsx
+++ src/pages/sales-service/SalesService.tsx
@@ -1,5 +1,29 @@
+import { createSignal, Show } from "solid-js";
 import "./salesService.css";
 
 export default function SalesService() {
-  return <>sales and service works</>;
+  return (
+    <div>
+      <AddNewButtonStack />
+    </div>
+  );
+}
+
+function AddNewButtonStack() {
+  const [isAddNewButtonClicked, setIsAddNewButtonClicked] =
+    createSignal<boolean>(false);
+
+  return (
+    <div>
+      <Show when={isAddNewButtonClicked()}>
+        <button>Sale</button>
+        <button>Service</button>
+      </Show>
+      <button
+        onclick={() => setIsAddNewButtonClicked(!isAddNewButtonClicked())}
+      >
+        {isAddNewButtonClicked() ? "close" : "Add"}
+      </button>
+    </div>
+  );
 }
