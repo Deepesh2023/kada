85be324f7bd91bdd50995843dc44a4b8681e25fa
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-11-13 10:59:57 +0530

    added an incomplete dialog window for the sales-service page for the add sale button action

--- src/pages/sales-service/SalesService.tsx
+++ src/pages/sales-service/SalesService.tsx
@@ -1,23 +1,31 @@
-import { createSignal, Show } from "solid-js";
+import { createEffect, createSignal, Show } from "solid-js";
 import "./salesService.css";
 
 export default function SalesService() {
+  const [showNewSaleDialog, setShowNewSaleDialog] = createSignal(false);
+  const [showNewServiceDialog, setShowNewServiceDialog] = createSignal(false);
+
   return (
-    <div>
-      <AddNewButtonStack />
+    <div class="sales-service-page">
+      <NewSaleDialogBox showNewSaleDialog={showNewSaleDialog} />
+
+      <AddNewButtonStack
+        setShowNewSaleDialog={setShowNewSaleDialog}
+        setShowNewServiceDialog={setShowNewServiceDialog}
+      />
     </div>
   );
 }
 
-function AddNewButtonStack() {
+function AddNewButtonStack({ setShowNewSaleDialog, setShowNewServiceDialog }) {
   const [isAddNewButtonClicked, setIsAddNewButtonClicked] =
     createSignal<boolean>(false);
 
   return (
     <div class="add-new-button-stack">
       <Show when={isAddNewButtonClicked()}>
-        <button>Sale</button>
-        <button>Service</button>
+        <button onclick={() => setShowNewSaleDialog(true)}>Sale</button>
+        <button onclick={() => setShowNewSaleDialog(true)}>Service</button>
       </Show>
       <button
         onclick={() => setIsAddNewButtonClicked(!isAddNewButtonClicked())}
@@ -26,4 +34,20 @@ function AddNewButtonStack() {
       </button>
     </div>
   );
+}
+
+function NewSaleDialogBox({ showNewSaleDialog }) {
+  let dialog;
+
+  createEffect(() => {
+    if (showNewSaleDialog() && dialog) {
+      dialog.showModal();
+    }
+  });
+
+  return <dialog ref={dialog}>hello</dialog>;
+}
+
+function NewServieDialogBox({ showNewServiceDialog }) {
+  return <dialog>hello</dialog>;
 }
