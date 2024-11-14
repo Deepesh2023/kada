f41aaae071333dd631d58fccb30923f75ec05406
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-11-14 06:10:09 +0530

    improved dialog box and started fresh

--- src/pages/sales-service/SalesService.tsx
+++ src/pages/sales-service/SalesService.tsx
@@ -1,46 +1,23 @@
-import { createEffect, createSignal, Show } from "solid-js";
+import { children, createSignal, JSX, Show } from "solid-js";
 import "./salesService.css";
-import { Accessor, Setter } from "solid-js/types/server/reactive.js";
 
 export default function SalesService() {
-  const [showNewSaleDialog, setShowNewSaleDialog] = createSignal(false);
-  const [showNewServiceDialog, setShowNewServiceDialog] = createSignal(false);
-
   return (
     <div class="sales-service-page">
-      <NewSaleDialogBox
-        showNewSaleDialog={showNewSaleDialog}
-        setShowNewSaleDialog={setShowNewSaleDialog}
-      />
-
-      <NewServieDialogBox
-        showNewServiceDialog={showNewServiceDialog}
-        setShowNewServiceDialog={setShowNewServiceDialog}
-      />
-
-      <AddNewButtonStack
-        setShowNewSaleDialog={setShowNewSaleDialog}
-        setShowNewServiceDialog={setShowNewServiceDialog}
-      />
+      <AddNewButtonStack />
     </div>
   );
 }
 
-function AddNewButtonStack({
-  setShowNewSaleDialog,
-  setShowNewServiceDialog,
-}: {
-  setShowNewSaleDialog: Setter<boolean>;
-  setShowNewServiceDialog: Setter<boolean>;
-}) {
+function AddNewButtonStack() {
   const [isAddNewButtonClicked, setIsAddNewButtonClicked] =
     createSignal<boolean>(false);
 
   return (
     <div class="add-new-button-stack">
       <Show when={isAddNewButtonClicked()}>
-        <button onclick={() => setShowNewSaleDialog(true)}>Sale</button>
-        <button onclick={() => setShowNewServiceDialog(true)}>Service</button>
+        <button>Sale</button>
+        <button>Service</button>
       </Show>
       <button
         onclick={() => setIsAddNewButtonClicked(!isAddNewButtonClicked())}
@@ -51,53 +28,10 @@ function AddNewButtonStack({
   );
 }
 
-function NewSaleDialogBox({
-  showNewSaleDialog,
-  setShowNewSaleDialog,
-}: {
-  showNewSaleDialog: Accessor<boolean>;
-  setShowNewSaleDialog: Setter<boolean>;
-}) {
+function DialogBox(props: { children: JSX.Element }) {
   let dialog: HTMLDialogElement | undefined;
 
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape" && showNewSaleDialog()) {
-      setShowNewSaleDialog(false);
-    }
-  });
+  const form = children(() => props.children);
 
-  createEffect(() => {
-    if (showNewSaleDialog() && dialog) {
-      dialog.showModal();
-    }
-  });
-
-  return <dialog ref={dialog}>hello</dialog>;
-}
-
-function NewServieDialogBox({
-  showNewServiceDialog,
-  setShowNewServiceDialog,
-}: {
-  showNewServiceDialog: Accessor<boolean>;
-  setShowNewServiceDialog: Setter<boolean>;
-}) {
-  let dialog: HTMLDialogElement | undefined;
-
-  document.addEventListener("keydown", (e) => {
-    if (
-      (e.key === "Escape" && showNewServiceDialog()) ||
-      showNewServiceDialog()
-    ) {
-      setShowNewServiceDialog(false);
-    }
-  });
-
-  createEffect(() => {
-    if (showNewServiceDialog() && dialog) {
-      dialog.showModal();
-    }
-  });
-
-  return <dialog ref={dialog}>hello</dialog>;
+  return <dialog ref={(el) => (dialog = el)}>{form()}</dialog>;
 }
