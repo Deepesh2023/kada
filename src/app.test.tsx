e130a7b295a0f3c3da23f4b93d2d63927a011898
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-11-19 16:10:18 +0530

    unneccessary gibberish

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,9 +1,13 @@
-import { render } from "@solidjs/testing-library";
-import { describe, test, expect } from "vitest";
+import { cleanup, render, screen } from "@solidjs/testing-library";
+import { describe, test, expect, beforeEach } from "vitest";
 
 import App from "./App";
 
 describe("Kada app", () => {
+  beforeEach(() => {
+    cleanup();
+  });
+
   test("Renders the app", () => {
     const app = render(() => <App />);
     expect(app).toBeTruthy();
