073cf5c3ac9cf0073af6c6b93421dddae68104cf
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-11-25 08:55:31 +0530

    added initial code for new session feature for sales page

--- src/pages/sales/Sales.tsx
+++ src/pages/sales/Sales.tsx
@@ -1,12 +1,27 @@
+import { Show, createSignal } from "solid-js";
+
 export default function Sales() {
+  const [showNewSession, setShowNewSession] = createSignal(false);
+
   return (
     <>
       <h2>sales</h2>
-      <div>
-        {/* yet to be implemented */}
-        <input type="checkbox" name="" id="" />
-        <label>Auto detect bar code scans</label>
-      </div>
+      <button onclick={() => setShowNewSession(true)}>New session</button>
+
+      <Show when={showNewSession()}>
+        <div>
+          <form>
+            {/* yet to be implemented */}
+            <input type="checkbox" name="" id="" />
+            <label>Auto detect bar code scans</label>
+
+            <div>
+              <button onclick={() => setShowNewSession(false)}>Close</button>
+              <button>Submit</button>
+            </div>
+          </form>
+        </div>
+      </Show>
     </>
   );
 }
