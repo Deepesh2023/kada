4fc4f0b1e7d0693d770318657b651c70c4f25716
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-11-25 09:30:55 +0530

    a basic form in Sales page for adding new sale and a basic table to list them out

--- src/pages/sales/Sales.tsx
+++ src/pages/sales/Sales.tsx
@@ -10,16 +10,42 @@ export default function Sales() {
 
       <Show when={showNewSession()}>
         <div>
-          <form>
-            {/* yet to be implemented */}
-            <input type="checkbox" name="" id="" />
-            <label>Auto detect bar code scans</label>
+          {/* yet to be implemented */}
+          <input type="checkbox" name="" id="" />
+          <label>Auto detect bar code scans</label>
 
-            <div>
-              <button onclick={() => setShowNewSession(false)}>Close</button>
-              <button>Submit</button>
-            </div>
+          <form action="">
+            <label for="product-select">Select product</label>
+            <select name="" id="product-select">
+              <option value="">Torch</option>
+              <option value="">battery</option>
+            </select>
+
+            <label for="product-quantity">Quantity</label>
+            <input type="number" id="prodcut-quantity" />
+
+            <label for="product-price">Price</label>
+            <input type="number" id="product-price" />
+
+            <button>Add</button>
           </form>
+
+          <table>
+            <tbody>
+              <tr>
+                <th>No</th>
+                <th>Product name</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Price X quantity</th>
+              </tr>
+            </tbody>
+          </table>
+
+          <div>
+            <button onclick={() => setShowNewSession(false)}>Close</button>
+            <button>Submit</button>
+          </div>
         </div>
       </Show>
     </>
